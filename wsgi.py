43202cffcf5295b41cd6aeb64e01a638db981bd1
aylegacyhostel <aylegacyhostel@gmail.com>
2026-09-06 10:54:44 -0400

    Install admin backup restore routes

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 import app as app_module
 from photo_storage import install_photo_storage
+from backup_restore import install_backup_restore
 
 install_photo_storage(app_module)
+install_backup_restore(app_module)
 app = app_module.app
